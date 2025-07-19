c0986659f641d5d1f99d4735d051e098e7375be3
Paulo Wilson Montori da Rocha <paulo.montori@gmail.com>
2025-07-19 18:28:56 -0300

    Emit new events after purchase

--- src/main/java/com/example/kafkaconsumer/KafkaConsumerService.java
+++ src/main/java/com/example/kafkaconsumer/KafkaConsumerService.java
@@ -2,15 +2,30 @@ package com.example.kafkaconsumer;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
 public class KafkaConsumerService {
     private static final Logger logger = LoggerFactory.getLogger(KafkaConsumerService.class);
 
+    private static final String ASSEMBLY_TOPIC = "assembly-line-topic";
+    private static final String INVENTORY_TOPIC = "inventory-topic";
+
+    private final KafkaTemplate<String, String> kafkaTemplate;
+
+    @Autowired
+    public KafkaConsumerService(KafkaTemplate<String, String> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
     @KafkaListener(topics = "example-topic", groupId = "example-group")
     public void listen(String message) {
         logger.info("Received message: {}", message);
+        kafkaTemplate.send(ASSEMBLY_TOPIC, message);
+        kafkaTemplate.send(INVENTORY_TOPIC, message);
+        logger.info("Emitted events to {} and {}", ASSEMBLY_TOPIC, INVENTORY_TOPIC);
     }
 }
